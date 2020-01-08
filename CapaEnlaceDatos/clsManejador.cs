851cd3f42e8c8a4c3cd6f9c0b4f003c346a28dc0
adrian <badrianj@gmail.com>
2020-01-07 23:22:14 -0400

    CapaEnlaceDatos terminada

---
+++ CapaEnlaceDatos/clsManejador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data; // importar
+using System.Data.SqlClient;
+
+namespace CapaEnlaceDatos
+{
+    public class clsManejador
+    {
+        public SqlConnection conexion = new SqlConnection("Server=TEAMLAPTOP90; DataBase=DemoNCapas; Integrated Security = True; User=sa; Password=8852893");
+        public void Conectar()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+        }
+        public void Desconectar()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+
+        public DataTable Listado(String NombrePS, List<clsParametro> lst) {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da;
+            try
+            {
+                Conectar();
+                da = new SqlDataAdapter(NombrePS, conexion);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                // Verificamos si la lista no esta vacía
+                if (lst != null)
+                {
+                    // Recorremos la lista Genérica
+                    for (int i = 0; i < lst.Count; i++)
+                    {
+                        // Pasamos cada uno de los parámetros
+                        da.SelectCommand.Parameters.AddWithValue(lst[i].Nombre, lst[i].Valor);
+                    }
+                }
+                // Llenamos nuestro DataTable
+                da.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            Desconectar();
+            return dt;
+        }
+    }
+}
---
+++ CapaEnlaceDatos/clsParametro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEnlaceDatos
+{
+    public class clsParametro
+    {
+        // Atritubos
+        private String m_Nombre;
+        private Object m_Valor;
+
+        // Propiedades
+        public String Nombre { get => m_Nombre; set => m_Nombre = value; }
+        public Object Valor { get => m_Valor; set => m_Valor = value; }
+
+        // Contrusctor
+        public clsParametro(String ObjNombre, Object objValor) {
+            m_Nombre = ObjNombre;
+            m_Valor = objValor;
+        }
+    }
+}
