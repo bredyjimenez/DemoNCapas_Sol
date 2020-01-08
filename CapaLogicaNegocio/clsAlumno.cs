e0edce885020751f57aa0f26cd830e3048716bfe
adrian <badrianj@gmail.com>
2020-01-07 23:33:12 -0400

    CapaLogicaNegocio terminada

---
+++ CapaLogicaNegocio/clsAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEnlaceDatos; // importar
+using System.Data;
+
+namespace CapaLogicaNegocio
+{
+    public class clsAlumno
+    {
+        private clsManejador M = new clsManejador();
+
+        public DataTable Listado()
+        {
+            return M.Listado("ListarAlumnos", null);
+        }
+    }
+}
